ac5ee1de27ea093d31d6e8842aed5201195b3ad5
Shriya Anand <73083755+sanand20@users.noreply.github.com>
2021-09-26 22:48:50 -0400

    Update printNameShriya.py
    changes after discussing with angela

--- printNameShriya.py
+++ printNameShriya.py
@@ -1,3 +1,7 @@
+#Shriya Anand and Angela Zhang
+# SoftDev
+# Classwork- Class Names
+# POW WOW SUMMARY: When you call the printName() function, it asks you which period you want the student from. Originally, the code would then provide a random student from the list; after talking to my partner, however, we have changed it so that the user can input which number student they want from the list.
 
 import random
 pd1 = ['emma', 'shriya', 'william']
@@ -5,12 +9,14 @@ pd2 = ['a', 'b', 'c']
 def printName():
     try: 
         period = int(input("Period 1 or 2? (only type number)"))
+        name_index = int (input("Choose a number from 0 to 2"))
         if period == 1:
-            print(pd1[random.randint(0,len(pd1)-1)])
+             print (pd1[name_index])
+            #print(pd1[random.randint(0,len(pd1)-1)])
         elif period == 2:
-            print (pd2[random.randint(0,len(pd2)-1)])
+             print (pd2[name_index])
+            #print (pd2[random.randint(0,len(pd2)-1)])
         else:
             print ("This input is not valid")
        
     except: print ("This input is not valid")    
-    
