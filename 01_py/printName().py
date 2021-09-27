8e21bd507e3e4e48788e35669e05998c63ca2db9
Shriya <>
2021-09-27 15:09:55 -0400

    added 01_py

---
+++ 01_py/printName().py
@@ -0,0 +1,14 @@
+import random
+pd1 = ['emma', 'shriya', 'william']
+pd2 = ['a', 'b', 'c']
+def printName():
+   try: 
+        period = int(input("Period 1 or 2? (only type number)"))
+        if period == 1:
+            print(pd1[random.randint(0,len(pd1)-1)])
+        elif period == 2:
+            print (pd2[random.randint(0,len(pd2)-1)])
+        else:
+            print ("This input is not valid")
+
+    except: print ("This input is not valid")    
