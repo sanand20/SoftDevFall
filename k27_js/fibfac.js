b0d298748d07e83779f6cb206b8d8b2ee8d1c07e
Shriya <>
2022-02-04 08:41:36 -0500

    added notes and fact

--- k27_js/fibfac.js
+++ k27_js/fibfac.js
@@ -1,4 +1,14 @@
+Team Mechanical Pencils :: Shriya Anand, Michael Borczuk
+SoftDev pd1
+K27 -- Basic functions in JavaScript
+2022-02-04
 
+var fact = function(n){
+	if (n <= 1){
+		return 1;
+	}
+	return (fact(n-1)*n);
+};
 
 var fib = function(n) {
 	if (n == 0){
