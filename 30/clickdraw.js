c38c49a1330ba1be103cb617baa83c234db16339
Shriya <>
2022-02-15 08:31:51 -0500

    code working!

--- 30/clickdraw.js
+++ 30/clickdraw.js
@@ -8,22 +8,32 @@ var toggleMode = (e) => {
   console.log("toggling...");
   if(mode === "rect"){
     mode = "circ";
+    buttonToggle.innerHTML = "Rectangle";
   }
   else{
     mode = "rect";
+    buttonToggle.innerHTML = "Circle";
   }
+  console.log(mode);
 };
 
 var drawRect = function(e){
-  var mouseX = e.clientX;
-  var mouseY = e.clientY;
-  ctx.fillRect(mouseX,mouseY, 20, 50);
+  var mouseX = e.offsetX;
+  var mouseY = e.offsetY;
+  // offsetX and offsetY - x/y distance from edge of element
+  // thus using them to draw the shape works
+  ctx.fillRect(mouseX, mouseY, 50, 100);
   console.log("mouse click registered at ", mouseX, mouseY);
 };
 
 var drawCircle = (e) => {
-  var mouseX = e.clientX + e.offsetX;
-  var mouseY =  e.offsetY-e.clientY;
+  var mouseX = e.offsetX;
+  var mouseY = e.offsetY;
+  ctx.beginPath();
+  ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
   console.log("mouse click registered at", mouseX, mouseY);
 };
 var draw = (e) => {
@@ -33,12 +43,13 @@ var draw = (e) => {
   else{drawCircle(e);}
 };
 
-// var wipeCanvas = () => {
-//
-// }
-//
+var wipeCanvas = () => {
+  ctx.clearRect(0,0,600,600);
+
+}
+
  c.addEventListener("click", draw);
-// var bToggler = document.;
-// bToggler. ;
-// var clearB = ;
-// clearB.;
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click",toggleMode) ;
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas);
